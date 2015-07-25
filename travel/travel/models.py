6e61b34709ea51a34c1b9698d80a7c2565d7b1f4
shashank <shashank@vunet.local>
2015-07-25 15:28:52 +0530

    making changes

---
+++ travel/travel/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.conf import settings
+
+class TravelUser(models.Model):
+	user_id = models.CharField(max_length = 50, blank=True, null=True)
+	name = models.CharField(max_length = 100, blank=True, null=True)
+	access_token = models.CharField(max_length=2000,blank=True, null=True)
