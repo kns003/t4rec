d14bd420f920b354dbaddccdd92c55fad1e3171d
shashank <shashank@vunet.local>
2015-07-25 16:47:27 +0530

    changes

--- travel/travel/wsgi.py
+++ travel/travel/wsgi.py
@@ -11,5 +11,8 @@ import os
 from dj_static import Cling
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travel.settings")
 
-from django.core.wsgi import get_wsgi_application
-application = Cling(get_wsgi_application())
+#from django.core.wsgi import get_wsgi_application
+#application = Cling(get_wsgi_application())
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
